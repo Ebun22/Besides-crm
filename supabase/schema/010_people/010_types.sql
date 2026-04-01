a4446b2c69d2b0f94fbd5145b61f6589699d1fd5
Ebun22 <subeydavid22@gmail.com>
2026-04-01 10:02:31 +0100

    created people types table

--- supabase/schema/010_people/010_types.sql
+++ supabase/schema/010_people/010_types.sql
@@ -1,6 +1,42 @@
-create table "public"."customer__types" (
-  id uuid not null default gen_random_uuid (),
-  name text not null,
-  description text null,
-  constraint contact__status_pkey primary key (id)
-)
+CREATE TABLE IF NOT EXISTS "public"."people__types" (
+  "id"          uuid NOT NULL DEFAULT gen_random_uuid (),
+  "name"        text NOT NULL,
+  "description" text     NULL,
+  CONSTRAINT people__types_pkey PRIMARY KEY ("id")
+);
+
+ALTER TABLE "public"."people__types" ENABLE ROW LEVEL SECURITY;
+
+-- RLS
+ALTER TABLE "public"."people__types" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users" ON "public"."people__types" FOR SELECT TO "authenticated" USING (true);
+
+-- CLS
+GRANT
+  REFERENCES,
+  TRIGGER,
+  TRUNCATE,
+  MAINTAIN
+ON TABLE
+  "public"."people__types"
+TO
+  "anon";
+
+GRANT
+  SELECT,
+  REFERENCES,
+  TRIGGER,
+  TRUNCATE,
+  MAINTAIN
+ON TABLE
+  "public"."people__types"
+TO
+  "authenticated";
+
+GRANT
+  ALL
+ON TABLE
+  "public"."people__types"
+TO
+  "service_role";
