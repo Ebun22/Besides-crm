20c05d803a6f0d148a1f32846a76da81e47cf197
Ebun22 <subeydavid22@gmail.com>
2026-04-01 09:45:17 +0100

    editted perms action

--- supabase/schema/000_permissions/020_actions.sql
+++ supabase/schema/000_permissions/020_actions.sql
@@ -1,6 +1,50 @@
-create table public.permission__roles (
-  id uuid not null default gen_random_uuid (),
-  role text not null,
-  description text null,
-  constraint pemission_roles_pkey primary key (id)
-) TABLESPACE pg_default;
+CREATE TABLE IF NOT EXISTS "public"."permission__actions" (
+  "id"          uuid NOT NULL DEFAULT gen_random_uuid(),
+  "action"      text NOT NULL,
+  "description" text NULL,
+  CONSTRAINT permission__actions_pkey PRIMARY KEY ("id")
+);
+
+ALTER TABLE "public"."permission__actions" OWNER TO "postgres";
+
+-- CLS
+GRANT
+  REFERENCES,
+  TRIGGER,
+  TRUNCATE,
+  MAINTAIN
+ON TABLE
+  "public"."permission__actions"
+TO
+ "anon";
+
+GRANT
+  SELECT,
+  REFERENCES,
+  TRIGGER,
+  TRUNCATE,
+  MAINTAIN
+ON TABLE
+  "public"."permission__actions"
+TO
+  "authenticated";
+
+GRANT
+  ALL
+ON TABLE
+  "public"."permission__actions"
+TO
+  "service_role";
+
+-- RLS
+ALTER TABLE "public"."permission__actions" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users"
+ON
+  "public"."permission__actions"
+FOR SELECT
+TO
+  "authenticated"
+USING (
+  true
+);
