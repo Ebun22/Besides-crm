ee2c72593559344989ca5ef532bfdc0e45f639fe
Ebun22 <subeydavid22@gmail.com>
2026-04-02 08:13:27 +0100

    created document types table

--- supabase/schema/050_document/010_types.sql
+++ supabase/schema/050_document/010_types.sql
@@ -1,4 +1,59 @@
-  virtual_bill text null,
-  prev_virtual_bill text null,
-  gdpr text null,
-    id_doc text null,
+CREATE TABLE IF NOT EXISTS "public"."document__types" (
+  "id"          uuid NOT NULL DEFAULT gen_random_uuid (),
+  "type"        text NOT NULL,
+  "description" text     NULL,
+  CONSTRAINT document__types_pkey PRIMARY KEY ("id")
+);
+
+ALTER TABLE "public"."document__types" OWNER TO "postgres";
+
+-- CLS
+REVOKE
+  ALL
+ON TABLE
+  "public"."document__types"
+FROM
+  "anon";
+
+GRANT
+  SELECT,
+  REFERENCES,
+  TRIGGER,
+  TRUNCATE,
+  MAINTAIN
+ON TABLE
+  "public"."permission__actions"
+TO
+  "authenticated";
+
+GRANT
+  ALL
+ON TABLE
+  "public"."document__types"
+TO
+  "service_role";
+
+-- RLS
+ALTER TABLE "public"."document__types" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users"
+ON
+  "public"."document__types"
+FOR SELECT
+TO
+  "authenticated"
+USING (
+  true
+);
+
+-- SEED
+INSERT INTO "public"."document__types"
+(
+  "name",
+  "description"
+)
+VALUES
+  (virtual_bill,       NULL),
+  (prev_virtual_bill,  NULL),
+  (GDPR,               NULL),
+  (IDENTIFICATION_DOC, NULL);
