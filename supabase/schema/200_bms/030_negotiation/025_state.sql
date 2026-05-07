3780d1372e9bb653e4545d1391edc1add412f329
Ebun22 <subeydavid22@gmail.com>
2026-05-07 13:42:36 +0100

    added the negotiation state table

---
+++ supabase/schema/200_bms/030_negotiation/025_state.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS "bms"."negotiation__states" (
+  "id"          uuid NOT NULL DEFAULT gen_random_uuid (),
+  "name"        text NOT NULL,
+  "description" text     NULL,
+  CONSTRAINT product__usage_categories_pkey PRIMARY KEY ("id")
+);
+
+ALTER TABLE "bms"."negotiation__states" ENABLE ROW LEVEL SECURITY;
+
+-- CLS
+REVOKE
+  ALL
+ON TABLE
+  "bms"."negotiation__states"
+FROM
+  "anon",
+  "authenticated";
+
+GRANT
+  SELECT
+ON TABLE
+  "bms"."negotiation__states"
+TO
+  "authenticated";
+
+-- RLS
+ALTER TABLE "bms"."negotiation__states" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY
+  "Enable read access for auth users"
+ON
+  "bms"."negotiation__states"
+FOR SELECT
+TO
+  "authenticated"
+USING (
+  true
+);
+
+-- SEED
+INSERT INTO "bms"."negotiation__states" (
+  "name",
+  "description"
+)
+VALUES
+  ('NEGOTATION_OPENED',           NULL),
+  ('NEGOTIATION_IN_PROGRESS',     NULL),
+  ('AWAITING_SIMULATION_RESULTS', NULL),
+  ('NEGOTATION_ACCEPTED',         NULL),
+  ('NEGOTATION_OPENED',           NULL),
+  ('NEGOTIATION_TERMINATED',      NULL);
