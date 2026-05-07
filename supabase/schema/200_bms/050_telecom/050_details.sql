84577a2e09217871fe540eaea40efb16610d3581
Ebun22 <subeydavid22@gmail.com>
2026-05-07 13:52:01 +0100

    added telecom details

---
+++ supabase/schema/200_bms/050_telecom/050_details.sql
@@ -0,0 +1,53 @@
+CREATE TABLE IF NOT EXISTS "bms"."telecom__details" (
+  "id"           uuid        NOT NULL DEFAULT gen_random_uuid (),
+  "tlc_supplier" varchar(60)     NULL,
+  "tlc_supplier" varchar(60)     NULL,
+  "msisdn"       bigint          NULL,
+  "msisdn"       text            NULL,
+  CONSTRAINT telecom__details_pkey PRIMARY KEY ("id")
+);
+
+ALTER TABLE "bms"."telecom__details" OWNER TO "postgres";
+
+-- CLS
+REVOKE
+  ALL
+ON TABLE
+  "bms"."telecom__details"
+FROM
+  "anon",
+  "authenticated";
+
+GRANT
+  SELECT,
+  INSERT,
+  UPDATE,
+  DELETE
+ON TABLE
+  "bms"."telecom__details"
+TO
+  "authenticated",
+  "service_role";
+
+-- RLS
+ALTER TABLE "bms"."telecom__details" ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Enable read access for all users"
+ON
+  "bms"."telecom__details"
+FOR SELECT
+TO
+  "authenticated"
+USING (
+  true
+);
+
+CREATE POLICY "Enable insert for authenticated users only"
+ON
+  "bms"."telecom__details"
+FOR INSERT
+TO
+  "authenticated"
+WITH CHECK (
+  true
+);
