d6ad95b276dba9b80cf8754dbf7c001a37c88fff
Ebun22 <subeydavid22@gmail.com>
2026-05-16 15:08:18 +0100

    changed how fixed amoun is found

--- src/services/OCR/extractText.ts
+++ src/services/OCR/extractText.ts
@@ -2,5 +2,6 @@ import pdfParse from 'pdf-parse';
 
 export async function extractTextFromPdf(buffer: Buffer): Promise<string> {
   const result = await pdfParse(buffer);
+  console.log("this is invoice result: ", result)
   return result.text ?? '';
 }
